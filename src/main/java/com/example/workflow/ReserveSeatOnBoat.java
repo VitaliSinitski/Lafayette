db472e346fbc4bb7bdf32c144abd481bfb44b88d
vitali.sin777@gmail.com <vitali.sin777@gmail.com>
2023-08-12 23:42:00 +0300

    1.0

---
+++ src/main/java/com/example/workflow/ReserveSeatOnBoat.java
@@ -0,0 +1,26 @@
+package com.example.workflow;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import javax.inject.Named;
+
+@Named
+public class ReserveSeatOnBoat implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        String money = "0.0";
+        String ticketType = "Coach";
+
+        money = (String) delegateExecution.getVariable("money");
+        double moneyDouble = Double.parseDouble(money);
+
+        if (moneyDouble >= 10000) {
+            ticketType = "First class";
+        } else if (moneyDouble >= 5000) {
+            ticketType = "Business class";
+        }
+
+        delegateExecution.setVariable("ticketType", ticketType);
+    }
+}
